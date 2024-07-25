272ccf18ba483232dc4c7d38f2ba8020cf7ce80a
LuoHuaZheng <164127610+LuoHuaZheng@users.noreply.github.com>
2024-07-25 16:08:56 +0800

    Update run.py

--- CFGKT/run.py
+++ CFGKT/run.py
@@ -15,24 +15,22 @@ def run(args):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     dtypes = {'uuid': 'int32',
               "upid": "int16", 'ucid': 'int16','is_correct': 'int8'}
-    train_df = pd.read_csv('./New_Log_Problem.csv', usecols=['uuid', 'upid', 'ucid', 'is_correct', 'timestamp_TW',
+    train_df = pd.read_csv('./Junyi.csv', usecols=['uuid', 'upid', 'ucid', 'is_correct', 'timestamp_TW',
                                                              'total_sec_taken'], dtype=dtypes, encoding='gbk')
     print("shape of dataframe :", train_df.shape)
-    raw_skill = train_df.ucid.unique().tolist()
-    raw_problem = train_df.upid.unique().tolist()
-    sub_skills = {p: i for i, p in enumerate(raw_skill)}
-    sub_problems = {p: i for i, p in enumerate(raw_problem)}
-    train_df['ucid'] = train_df['ucid'].map(sub_skills)
-    train_df['upid'] = train_df['upid'].map(sub_problems)
-    skills = train_df.upid.unique()
-    n_skills = len(skills)
+    raw_concept = train_df.ucid.unique().tolist()
+    raw_question = train_df.upid.unique().tolist()
+    sub_concept = {p: i for i, p in enumerate(raw_concept)}
+    sub_question = {p: i for i, p in enumerate(raw_question)}
+    train_df['ucid'] = train_df['ucid'].map(sub_concept)
+    train_df['upid'] = train_df['upid'].map(sub_question)
+    n_ques = len(train_df.upid.unique())
     n_cats = len(train_df.ucid.unique())
-    print("no. of skills :", n_skills)
-    print("no. of categories: ", n_cats)
+    print("no. of questions :", n_ques)
+    print("no. of concepts: ", n_cats)
     print("shape after exlusion:", train_df.shape)
 
     train_df['total_sec_taken'] = train_df['total_sec_taken'].clip(0, 300)
-
     train_df['timestamp_TW'] = train_df['timestamp_TW'].str[:-4]
     train_df['timestamp_TW'] = pd.to_datetime(train_df['timestamp_TW'])
     train_df['timestamp_TW'] = train_df['timestamp_TW'].astype(int) / (10 ** 9)
@@ -46,28 +44,25 @@ def run(args):
     train, val = train_test_split(group, test_size=0.2)
     val, test = train_test_split(val,test_size=0.5)
     print("train size: ", train.shape, "validation size: ", val.shape, "Test size: ",test.shape)
-    train_dataset = CFGKT_dataset(train.values, n_skills=n_skills, n_concept=n_cats, max_seq=args.max_len)
-    val_dataset = CFGKT_dataset(val.values, n_skills=n_skills, n_concept=n_cats, max_seq=args.max_len)
-    test_dataset = CFGKT_dataset(test.values, n_skills=n_skills, n_concept=n_cats, max_seq=args.max_len)
+    train_dataset = CFGKT_dataset(train.values, n_ques=n_ques, n_concept=n_cats, max_seq=args.max_len)
+    val_dataset = CFGKT_dataset(val.values, n_ques=n_ques, n_concept=n_cats, max_seq=args.max_len)
+    test_dataset = CFGKT_dataset(test.values, n_ques=n_ques, n_concept=n_cats, max_seq=args.max_len)
     train_loader = DataLoader(train_dataset,
                               batch_size=args.batch_size,
                               num_workers=2,
-                              shuffle=True,
-                              worker_init_fn=np.random.seed(seed=300))
+                              shuffle=True)
     val_loader = DataLoader(val_dataset,
                             batch_size=args.batch_size,
                             num_workers=2,
-                            shuffle=False,
-                            worker_init_fn=np.random.seed(seed=300))
+                            shuffle=False)
     test_loader = DataLoader(test_dataset,
                              batch_size=args.batch_size,
                              num_workers=2,
-                             shuffle=False,
-                             worker_init_fn=np.random.seed(seed=300))
+                             shuffle=False)
 
     del train_dataset, val_dataset, test_dataset
     gc.collect()
-    KT_model = CFGKT(n_question=n_cats, n_pid=n_skills, d_model=args.embed_dim, n_blocks=args.n_blocks,
+    KT_model = CFGKT(n_cats=n_cats, n_pid=n_ques, d_model=args.embed_dim, n_blocks=args.n_blocks,
                  kq_same = args.kq_same, dropout=args.dropout, model_type='CFGKT',memory_size=args.memory_size,
                final_fc_dim=args.final_fc_dim,n_heads=args.n_heads,d_ff=args.d_ff,
                time=args.time,interval=args.interval)
@@ -101,7 +96,7 @@ if __name__ == "__main__":
                             required=False)
     arg_parser.add_argument("--batch_size",
                             dest="batch_size",
-                            default=32,
+                            default=256,
                             type=int,
                             required=False)
     arg_parser.add_argument("--time",
@@ -141,7 +136,7 @@ if __name__ == "__main__":
                             required=False)
     arg_parser.add_argument("--epoch",
                             dest="epoch",
-                            default=100,  # 15
+                            default=100, 
                             type=int,
                             required=False)
     arg_parser.add_argument("--max_len",
