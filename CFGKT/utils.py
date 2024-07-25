8c606a0e09c8c31edd2ea88ceb78080f5713a56c
LuoHuaZheng <164127610+LuoHuaZheng@users.noreply.github.com>
2024-07-25 16:00:16 +0800

    Update utils.py

--- CFGKT/utils.py
+++ CFGKT/utils.py
@@ -5,7 +5,7 @@ import random
 import os
 
 class EarlyStopping():
-    def __init__(self, patience=5, verbose=False, delta=0):
+    def __init__(self, patience=7, verbose=False, delta=0):
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
@@ -67,8 +67,8 @@ def eval_one_epoch(model, val_iterator, device,loss_function):
     stop_sign = False
     model.eval()
     with torch.no_grad():
-        preds_no_first = []
-        truths_no_first = []
+        preds = []
+        truths = []
         for i, (qa, qid, labels, mask, concept, dotime, lagtime) in enumerate(val_iterator):
             qa, qid, labels, mask, concept, dotime, lagtime = (qa.to(device), qid.to(device), labels.to(device),
                                                               mask.to(device), concept.to(device),
@@ -76,20 +76,19 @@ def eval_one_epoch(model, val_iterator, device,loss_function):
 
             pred = model(concept, qa, labels, qid, dotime, lagtime)
             loss = loss_function(pred, labels, mask)
-            print('vallllllllll----loss  :', loss.item())
-            truth_no_first, pred_no_first = values_after_mask(pred, labels, mask)
-            truths_no_first.append(truth_no_first)
-            preds_no_first.append(pred_no_first)
+            print('val----loss  :', loss.item())
+            truth, pred = values_after_mask(pred, labels, mask)
+            truths.append(truth)
+            preds.append(pred)
 
-        truths_no_first = np.concatenate(truths_no_first)
-        preds_no_first = np.concatenate(preds_no_first)
+        truths = np.concatenate(truths)
+        preds = np.concatenate(preds)
 
-        auc_no_first = roc_auc_score(truths_no_first, preds_no_first)
-        acc_no_first = accuracy_score(truths_no_first, preds_no_first.round())
-        f1_no_last = f1_score(truths_no_first, preds_no_first.round())
-        print("\nvalllllllauc_no_last=%.4f acc_no_last=%.4f f1_no_last=%.4f" % (
-        auc_no_first, acc_no_first, f1_no_last))
-        early_stopping(auc_no_first*(-1), model)
+        auc = roc_auc_score(truths, preds)
+        acc = accuracy_score(truths, preds.round())
+        f1 = f1_score(truths, preds.round())
+        print("\nval-auc=%.4f acc=%.4f f1=%.4f" % (auc, acc, f1))
+        early_stopping(auc*(-1), model)
         if early_stopping.early_stop:
             print("stop training!!!!!!!!")
             stop_sign = True
@@ -99,8 +98,8 @@ def eval_one_epoch(model, val_iterator, device,loss_function):
 def test_one_epoch(model, test_iterator, device):
     model.eval()
     with torch.no_grad():
-        preds_no_first = []
-        truths_no_first = []
+        preds = []
+        truths = []
 
         for i, (qa, qid, labels, mask, concept, dotime, lagtime) in enumerate(test_iterator):
             qa, qid, labels, mask, concept, dotime, lagtime = (qa.to(device), qid.to(device), labels.to(device),
@@ -108,18 +107,17 @@ def test_one_epoch(model, test_iterator, device):
                                                                dotime.to(device), lagtime.to(device))
             with torch.no_grad():
                 pred = model(concept, qa, labels, qid, dotime, lagtime)
-            truth_no_first, pred_no_first = values_after_mask(pred, labels, mask)
-            truths_no_first.append(truth_no_first)
-            preds_no_first.append(pred_no_first)
+            truth, pred = values_after_mask(pred, labels, mask)
+            truths.append(truth)
+            preds.append(pred)
 
-        truths_no_first = np.concatenate(truths_no_first)
-        preds_no_first = np.concatenate(preds_no_first)
-        mse = mean_squared_error(truths_no_first,preds_no_first)
-        auc_no_first = roc_auc_score(truths_no_first, preds_no_first)
-        acc_no_first = accuracy_score(truths_no_first, preds_no_first.round())
-        f1_no_last = f1_score(truths_no_first, preds_no_first.round())
-        print("\nauc_no_last=%.4f acc_no_last=%.4f f1_no_last=%.4f" % (
-        auc_no_first, acc_no_first, f1_no_last))
+        truths = np.concatenate(truths)
+        preds = np.concatenate(preds)
+        mse = mean_squared_error(truths,preds)
+        auc = roc_auc_score(truths, preds)
+        acc = accuracy_score(truths, preds.round())
+        f1 = f1_score(truths, preds.round())
+        print("\nauc=%.4f acc=%.4f f1=%.4f" % (auc, acc, f1))
         print('MSE',mse)
 
 def values_after_mask(pred, labels, mask):
@@ -128,3 +126,15 @@ def values_after_mask(pred, labels, mask):
     pred_except_first = torch.masked_select(pred.view(-1), mask_except_first).detach().cpu().numpy()
     truth_except_first = torch.masked_select(labels.view(-1), mask_except_first).detach().cpu().numpy()
     return truth_except_first, pred_except_first
+
+def ut_mask(seq_len):
+    return torch.triu(torch.ones(seq_len,seq_len),diagonal=1).to(dtype=torch.bool).to(device)
+
+def lt_mask(seq_len):
+    return torch.tril(torch.ones(seq_len,seq_len),diagonal=-1).to(dtype=torch.bool).to(device)
+
+def pos_encode(seq_len):
+    return torch.arange(seq_len).unsqueeze(0).to(device)
+
+def get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
